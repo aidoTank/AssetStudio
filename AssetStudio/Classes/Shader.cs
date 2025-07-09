bb810ef8400d436076ff3b5e71305456314b59ba
simon <simon@staruniongame.com>
2025-07-09 14:16:49 +0800

    fix: remove duplicate  class

--- AssetStudio/Classes/Shader.cs
+++ AssetStudio/Classes/Shader.cs
@@ -706,33 +706,6 @@ namespace AssetStudio
         }
     }
 
-    public class SerializedPlayerSubProgram
-    {
-        public uint m_BlobIndex;
-        public ushort[] m_KeywordIndices;
-        public long m_ShaderRequirements;
-        public ShaderGpuProgramType m_GpuProgramType;
-
-        public SerializedPlayerSubProgram(ObjectReader reader)
-        {
-            m_BlobIndex = reader.ReadUInt32();
-
-            int numKeywordIndices = reader.ReadInt32();
-            m_KeywordIndices = new ushort[numKeywordIndices];
-            for (int i = 0; i < numKeywordIndices; i++)
-            {
-                m_KeywordIndices[i] = reader.ReadUInt16();
-            }
-
-            reader.AlignStream();
-
-            m_ShaderRequirements = reader.ReadInt64();
-            m_GpuProgramType = (ShaderGpuProgramType)reader.ReadSByte();
-            
-            reader.AlignStream();
-        }
-    }
-
     public class SerializedProgram
     {
         public SerializedSubProgram[] m_SubPrograms;
