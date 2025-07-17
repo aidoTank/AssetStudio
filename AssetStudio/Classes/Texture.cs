9649be3e96d08078795027aa54fd1b30710244c8
simon <simon@staruniongame.com>
2025-07-17 17:11:30 +0800

    fix:  6000 Texture support

--- AssetStudio/Classes/Texture.cs
+++ AssetStudio/Classes/Texture.cs
@@ -9,7 +9,12 @@ namespace AssetStudio
     {
         protected Texture(ObjectReader reader) : base(reader)
         {
-            if (version[0] > 2017 || (version[0] == 2017 && version[1] >= 3)) //2017.3 and up
+            if (version[0] >= 2023) // 2023.1 and up (6000 avaliable 2023 not tested)
+            {
+                var m_IsAlphaChannelOptional = reader.ReadBoolean();
+                reader.AlignStream();
+            }
+            else if (version[0] > 2017 || (version[0] == 2017 && version[1] >= 3)) //2017.3 to 2022.x
             {
                 var m_ForcedFallbackFormat = reader.ReadInt32();
                 var m_DownscaleFallback = reader.ReadBoolean();
